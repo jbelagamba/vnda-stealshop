661f522b78d0e95069617bb46da6e21cdf3fe82f
Jaqueline Belagamba <jaquelinebdemoraes@gmail.com>
2019-01-05 15:47:42 -0200

    LAYOUT: Adiciona efeitos e refatora codigo.
     * Adiciona mouseover nos destaques e news
     * Adiciona CSS de animação
     * Adiciona fonte Open Sans
     * Otimiza refatora trechos do codigo sass
     * Remove arquivos desnecessarios
     * Retira js no index.html
     * Modifica setas do slick no mobile

---
+++ src/js/99-custom.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    /*Instancia Menu*/
+    $('nav#menu').mmenu();
+
+    /*Instancia Slick para diferentes tamanhos de tela*/
+    $('.slider-products').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+});
+
+/*Efeito mouseover no destaque*/
+$(".img-destaque").mouseover(function(){
+    $(this).removeClass("zoomOutSteal");
+    $(this).removeClass("animated");
+    $(this).addClass("zoomInSteal");
+    $(this).addClass("animated");
+});
+
+$(".img-destaque").mouseleave(function(){
+    $(this).removeClass("zoomInSteal");
+    $(this).removeClass("animated");
+    $(this).addClass("zoomOutSteal");
+    $(this).addClass("animated");
+});
+
+/* Efeito da section News*/
+$(".image-product").mouseover(function(){
+    msk  = $(this).find('.msk');
+    msk.fadeIn();
+    back  = $(this).find('.back');
+    back.fadeIn();
+});
+
+$(".image-product").mouseleave(function(){
+    msk  = $(this).find('.msk');
+    msk.fadeOut();
+    back  = $(this).find('.back');
+    back.fadeOut();
+});
+
